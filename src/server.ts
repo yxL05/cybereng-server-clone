85debce77cc1abbfd388d37be921adf3edd846ab
yxL05 <yang.xiang.64@hotmail.com>
2025-07-25 16:14:30 -0400

    Configured .env.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import productRouter from './routes/product.js'
+import dotenv from 'dotenv';
+import { getEnv } from './util/dotenv.js';
+
+dotenv.config();
 
 const app = express();
 
 app.use('/products', productRouter);
 
-const PORT: number = 8080;
+const PORT: number = parseInt(getEnv('PORT')) || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ src/util/dotenv.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'; dotenv.config();
+
+const getEnv = (key: string): string => {
+  const val = process.env[key]
+  if (!val) throw new Error(`.env: missing required ${key} key.`)
+  return val;
+} 
+  
+
+export {
+  getEnv
+}
